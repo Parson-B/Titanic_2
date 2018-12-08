a503521e60b062a711039084cfa364bb138194a5
Parson-B <dasd2154@abwesend.de>
2018-12-09 00:28:10 +0100

    Added further graphs to identify potentially good predictors

--- Code_Snippets.py
+++ Code_Snippets.py
@@ -78,3 +78,72 @@ Res = pd.DataFrame(tmp[1:],columns=tmp[0])
 
 sns.jointplot(x='Fare', y='Survival Pct', data=Res).savefig('Plots/JointPlot_surv_by_fare.png')
 sns.jointplot(x='Fare', y='Survival Pct', data=Res, kind='hex',gridsize=10).savefig('Plots/JointPlot_surv_by_fare_hex.png')
+
+
+#Perform a binary logistic regression to indentify the prediction quality of features
+from sklearn import preprocessing
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import train_test_split
+
+#Check for possible adaption to increase sample sizes
+training.Survived.value_counts()
+
+sns.countplot(x='Survived',data=training,palette='hls')
+plt.title('Absolute Survivors and Deceased')
+plt.ylabel('Count')
+plt.savefig('Plots/Survived_counts.png')
+
+# Categories ['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp','Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'] 
+# Examine 'Survived' and consider first ['Pclass', 'Sex', 'Age', 'Fare']
+
+for cat in ['Pclass', 'Sex', 'Age', 'Fare']:
+    print(training[cat].unique())
+
+#Analyse absolut survival numbers by class
+pd.crosstab(training.Pclass,training.Survived).plot(kind='bar')
+plt.title('Survival Number by Passenger Class')
+plt.xlabel('Passenger Class')
+plt.ylabel('Number of Survivors')
+plt.savefig('Plots/BarPlot_surv_by_class_abs.png')
+
+#Show stacked as percentage
+table=pd.crosstab(training.Pclass,training.Survived)
+table.div(table.sum(1).astype(float), axis=0).plot(kind='bar', stacked=True)
+plt.title('Stacked Bar Chart Survival by Class')
+plt.xlabel('Passgenger Class')
+plt.ylabel('Proportion')
+plt.savefig('Plots/StackedBarPlot_surv_by_class_pct.png')
+
+#Create Passenger Age Histogram
+training.Age.hist()
+plt.title('Histogram of Age')
+plt.xlabel('Age')
+plt.ylabel('Frequency')
+plt.savefig('Plots/Histogram_Age.png')
+
+#Analyse absolut survival numbers by Age
+#First create age cohorts in intervalls of ten years
+tmp_df=training[['Age','Survived']]
+tmp_age_groups=pd.DataFrame([(tmp_df['Age'] // 10) * 10]).transpose()
+tmp_age_groups.columns=['Age Group']
+tmp_df=pd.concat([tmp_df,tmp_age_groups],axis=1)
+
+pd.crosstab(tmp_df['Age Group'],tmp_df['Survived']).plot(kind='bar')
+plt.title('Survival Number by Age Groups')
+plt.xlabel('Age Group')
+plt.ylabel('Number of Survivors')
+plt.savefig('Plots/BarPlot_surv_by_Age_Groups_abs.png')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
